2808c42dfd56e298afb72752b5ee5228f060abab
Tamas Nora <tamasnora94@gmail.com>
2021-06-24 19:41:41 +0200

    add feladat_szovegek

--- selenium-homework/seleniumex.py
+++ selenium-homework/seleniumex.py
@@ -1,3 +1,13 @@
+'''
+008 Feladat: Hibakezelés megvalósítása selenium programban
+
+A feladatokat külön python fileban oldd meg. Minden feladat tartalmazza az elvárt filenevet. Ezen a néven fogadható el a megoldás.
+
+    Készíts egy Python alkalmazást ami selenium-ot használ. Nyisson meg egy Chrome böngészöt és töltsön be egy tetszőleges weblapot az Internetről. Az oldalon probáld megtalálni a <div id="nemletezik"></div> mezőt. A lényeg, hogy hibát dogjon a driver.find_by_id függvény hívás. Feladatot, hogy kezed le ezt a hibát és írj ki valami emberileg olvasható üzenetet. Extra feladatként készíts egy saját függvényt, ami bármilyen find_by_id lokátor hívásnál lekezeli a nemlétező elem tipusú hibát. A megoldáshoz használj python try except struktúrát.
+
+        A megoldást egy seleniumex.py nevű fileban kell beadnod.
+'''
+
 from selenium import webdriver
 
 path = ("C:\\Windows\\chromedriver.exe")
--- selenium-homework/start.py
+++ selenium-homework/start.py
@@ -1,8 +1,19 @@
+'''
+Feladat: Selenium weboldal menyitás gyakorlása
+
+A feladatokat külön python fileban oldd meg. Minden feladat tartalmazza az elvárt filenevet. Ezen a néven fogadható el a megoldás.
+
+    Készíts egy Python alkalmazást ami selenium-ot használ. Nyisson meg egy Chrome böngészöt és töltsön be egy tetszőleges weblapot az Internetről.
+
+        A megoldást egy start.py nevű fileban kell beadnod.
+'''
+
 from selenium import webdriver
 
-url = "C:\\Windows\\chromedriver.exe"
-browser = webdriver.Chrome()
+path = "C:\\Windows\\chromedriver.exe"
+url = "https://www.google.com/"
+browser = webdriver.Chrome(path)
 
-browser.get("https://www.google.com/")
+browser.get(url)
 browser.quit()
 
