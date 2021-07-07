32a5c24520e117bcdaa3cf656f6f1a8c0bab442e
Tamas Nora <tamasnora94@gmail.com>
2021-07-07 21:19:29 +0200

    add substract.py

---
+++ python-homework/substract.py
@@ -0,0 +1,9 @@
+'''
+Feladat: Python Adatok és változók feladat
+1. Kérjünk be két számot, és írjuk ki a különbségüket.
+Használd az input() és print() beépített függvényeket.
+'''
+
+number1 = input("Adj meg egy számot: ")
+number2 = input("Adj meg még egy számot: ")
+print("A két szám különsége:", str(int(number1) - int(number2)))
